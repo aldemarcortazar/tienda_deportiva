17c164a7ca4a91c7e0f5e9d949adf264b5aaba8a
aldemar niño cortazar <anino79@misena.edu.co>
2021-03-26 08:56:10 -0500

    cambios en el login

---
+++ api/api/estadisticas.php
@@ -0,0 +1,11 @@
+<?php
+require_once './api/classes/class_estadistica.php';
+header('Content-Type: application/json');
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'GET':
+        
+        break;
+    case 'POST':
+        break;
+}
