7248458e09fd862a97fcb45f6c5eb1dbeb78f3a7
LuisGarcia <lmgarciaa@ut.edu.co>
2021-03-26 12:54:32 -0500

    reducir codigo

--- api/classes/class_estadistica.php
+++ api/classes/class_estadistica.php
@@ -4,173 +4,83 @@ require_once '../classes/conexion.php';
 // clase estadistica, para hacer automatizacion
 
 class Estadistica extends Conecction{
-    private $documento;
-    private $nombres;
-    private $apellidos;
-    private $fecha_venta;
-    private $fecha_creacion;
-    private $id_tipo_docu;
-    private $nombre_almacen;
-    private $nom_prenda;
-    private $precio;
-    private $talla;
-    private $cantidad;
-    private $valor_total_venta;
-    private $nombre_pago;
-    private $detalle_venta;
-    private $id_venta_enca;
-    private $id_prenda;
-
-
+    private $documento; private $nombres; private $apellidos; private $fecha_venta; private $fecha_creacion; private $id_tipo_docu;
+    private $nombre_almacen; private $nom_prenda; private $precio; private $talla; private $cantidad; private $valor_total_venta;
+    private $nombre_pago; private $detalle_venta; private $id_venta_enca; private $id_prenda;
 
     public function __construct($documento = null, $nombres = null, $apellidos = null, $fecha_venta = null, $fecha_creacion = null, $id_tipo_docu = null, $nombre_almacen = null, $nom_prenda = null, $precio = null, $talla = null, $cantidad = null, $valor_total_venta = null, $nombre_pago = null, $detalle_venta = null, $id_venta_enca = null, $id_prenda = null)
     {
         parent::__construct();
-        $this->documento = $documento;
-        $this->nombres = $nombres;
-        $this->apellidos = $apellidos;
-        $this->fecha_venta = $fecha_venta;
-        $this->fecha_creacion = $fecha_creacion;
-        $this->id_tipo_docu = $id_tipo_docu;
-        $this->nombre_almacen = $nombre_almacen;
-        $this->nom_prenda = $nom_prenda;
-        $this->precio = $precio;
-        $this->talla = $talla;
-        $this->cantidad = $cantidad;
-        $this->valor_total_venta = $valor_total_venta;
-        $this->nombre_pago = $nombre_pago;
-        $this->detalle_venta = $detalle_venta;
-        $this->id_venta_enca = $id_venta_enca;
-        $this->id_prenda = $id_prenda;
+        $this->documento = $documento; $this->nombres = $nombres; $this->apellidos = $apellidos; $this->fecha_venta = $fecha_venta;
+        $this->fecha_creacion = $fecha_creacion; $this->id_tipo_docu = $id_tipo_docu; $this->nombre_almacen = $nombre_almacen;
+        $this->nom_prenda = $nom_prenda; $this->precio = $precio; $this->talla = $talla; $this->cantidad = $cantidad;
+        $this->valor_total_venta = $valor_total_venta; $this->nombre_pago = $nombre_pago; $this->detalle_venta = $detalle_venta;
+        $this->id_venta_enca = $id_venta_enca; $this->id_prenda = $id_prenda;
     }
 
-    public function getDocumento(){
-        return $this->documento;
-    }
+    public function getDocumento(){ return $this->documento; }
 
-    public function setDocumento($documento){
-        $this->documento = $documento;
-    }
+    public function setDocumento($documento){ $this->documento = $documento; }
 
-    public function getNombres(){
-        return $this->nombres;
-    }
+    public function getNombres(){ return $this->nombres; }
 
-    public function setNombres($nombres){
-        $this->nombres = $nombres;
-    }
+    public function setNombres($nombres){ $this->nombres = $nombres; }
 
-    public function getApellidos(){
-        return $this->apellidos;
-    }
+    public function getApellidos(){ return $this->apellidos; }
 
-    public function setApellidos($apellidos){
-        $this->apellidos = $apellidos;
-    }
+    public function setApellidos($apellidos){ $this->apellidos = $apellidos; }
 
-    public function getFecha_venta(){
-        return $this->fecha_venta;
-    }
+    public function getFecha_venta(){ return $this->fecha_venta; }
 
-    public function setFecha_venta($fecha_venta){
-        $this->fecha_venta = $fecha_venta;
-    }
+    public function setFecha_venta($fecha_venta){ $this->fecha_venta = $fecha_venta; }
 
-    public function getFecha_Creacion(){
-        return $this->fecha_creacion;
-    }
+    public function getFecha_Creacion(){ return $this->fecha_creacion; }
 
-    public function setFecha_Creacion($fecha_creacion){
-        $this->fecha_creacion = $fecha_creacion;
-    }
+    public function setFecha_Creacion($fecha_creacion){ $this->fecha_creacion = $fecha_creacion; }
 
-    public function getId_tipo_docu(){
-        return $this->id_tipo_docu;
-    }
+    public function getId_tipo_docu(){ return $this->id_tipo_docu; }
 
-    public function setId_tipo_docu($id_tipo_docu){
-        $this->id_tipo_docu = $id_tipo_docu;
-    }
+    public function setId_tipo_docu($id_tipo_docu){ $this->id_tipo_docu = $id_tipo_docu; }
 
-    public function getNom_almacen(){
-        return $this->nombre_almacen;
-    }
+    public function getNom_almacen(){ return $this->nombre_almacen; }
 
-    public function setNom_almacen($nombre_almacen){
-        $this->nombre_almacen = $nombre_almacen;
-    }
+    public function setNom_almacen($nombre_almacen){ $this->nombre_almacen = $nombre_almacen; }
 
-    public function getNom_Prenda(){
-        return $this->nom_prenda;
-    }
+    public function getNom_Prenda(){ return $this->nom_prenda; }
 
-    public function setNom_Prenda($nom_prenda){
-        $this->nom_prenda = $nom_prenda;
-    }
+    public function setNom_Prenda($nom_prenda){ $this->nom_prenda = $nom_prenda; }
 
-    public function getPrecio(){
-        return $this->precio;
-    }
+    public function getPrecio(){ return $this->precio; }
 
-    public function setPrecio($precio){
-        $this->precio = $precio;
-    }
+    public function setPrecio($precio){ $this->precio = $precio; }
 
-    public function getTalla(){
-        return $this->talla;
-    }
+    public function getTalla(){ return $this->talla; }
 
-    public function setTalla($talla){
-        $this->talla = $talla;
-    }
+    public function setTalla($talla){ $this->talla = $talla; }
 
-    public function getCantidad(){
-        return $this->cantidad;
-    }
+    public function getCantidad(){ return $this->cantidad; }
 
-    public function setCantidad($cantidad){
-        $this->cantidad = $cantidad;
-    }
+    public function setCantidad($cantidad){ $this->cantidad = $cantidad; }
 
-    public function getValor_total_venta(){
-        return $this->valor_total_venta;
-    }
+    public function getValor_total_venta(){ return $this->valor_total_venta; }
 
-    public function setValor_total_venta($valor_total_venta){
-        $this->valor_total_venta = $valor_total_venta;
-    }
+    public function setValor_total_venta($valor_total_venta){ $this->valor_total_venta = $valor_total_venta; }
 
-    public function getNom_Pago(){
-        return $this->nombre_pago;
-    }
+    public function getNom_Pago(){ return $this->nombre_pago; }
 
-    public function setNom_Pago($nombre_pago){
-        $this->nombre_pago = $nombre_pago;
-    }
+    public function setNom_Pago($nombre_pago){ $this->nombre_pago = $nombre_pago; }
 
-    public function getDetalle_venta(){
-        return $this->detalle_venta;
-    }
+    public function getDetalle_venta(){ return $this->detalle_venta; }
 
-    public function setDetalle_venta($detalle_venta){
-        $this->detalle_venta = $detalle_venta;
-    }
+    public function setDetalle_venta($detalle_venta){ $this->detalle_venta = $detalle_venta; }
 
-    public function getVenta_encabezado(){
-        return $this->id_venta_enca;
-    }
+    public function getVenta_encabezado(){ return $this->id_venta_enca; }
 
-    public function setVenta_encabezado($id_venta_enca){
-        $this->id_venta_enca = $id_venta_enca;
-    }
+    public function setVenta_encabezado($id_venta_enca){ $this->id_venta_enca = $id_venta_enca; }
 
-    public function getId_prenda(){
-        return $this->id_prenda;
-    }
+    public function getId_prenda(){ return $this->id_prenda; }
 
-    public function setId_prenda($id_prenda){
-        $this->id_prenda = $id_prenda;
-    }
+    public function setId_prenda($id_prenda){ $this->id_prenda = $id_prenda; }
 
     public function getAllFactura(){
         try{
